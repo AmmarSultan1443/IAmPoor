84041a49a87628c458e9bf5557d13ed2c1d0f416
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2024-01-19 15:09:14 +0100

    Add AL MISBAH INFORMATIQUE logo.

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,38 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('I Am Poor'),
+          title: const Text(
+            'I Am Poor',
+            style: TextStyle(color: Colors.white),
+          ),
           backgroundColor: Colors.blueGrey[900],
         ),
         backgroundColor: Colors.blueGrey,
-        body: const Center(
-          child:
-          Image(
-            image: AssetImage('images/coal2.png'),
-        ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  padding: EdgeInsets.all(20.0),
+                  color: Colors.white,
+                  child: Image.asset('images/logoAIM.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: const Center(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Image(
+                    image: AssetImage('images/coal2.png'),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     ),
